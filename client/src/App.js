6a7128e1ed063ddbabcb3c5419cab29be5139d20
Diana Mayhugh <dianamayhugh@gmail.com>
2020-08-24 12:58:19 -0400

    app test

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
-import Books from "./pages/Books";
-import Nav from "./components/Nav";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 
-function App() {
-  return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
-  );
+import NoMatch from "./pages/NoMatch";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
